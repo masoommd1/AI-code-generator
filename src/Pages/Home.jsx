64349ee7c537133262df6b66fd2c5d2e7cd68361
Md Masoom <120587384+masoommd1@users.noreply.github.com>
2025-10-25 23:11:12 +0545

    ui design done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Select from "react-select";
 import { BsStars } from "react-icons/bs";
-
+import { HiOutlineCode } from "react-icons/hi";
 
 const Home = () => {
   const options = [
@@ -34,23 +34,30 @@ const Home = () => {
             Describe Your Components
           </p>
           <textarea
-            className="w-full min-h-[250px] bg-[#111827] mt-3 rounded-xl p-2.5  hover:border-2 border-purple-800 "
+            className="w-full min-h-[250px] bg-[#111827] mt-3 rounded-xl p-2.5  hover:border  border-purple-500 "
             placeholder="Describe Your Components in detail"
           ></textarea>
           <div className="flex items-center justify-between px-2.5">
-          <p className="text-gray-500 text-[17px] font-semibold">Click to Generate the Code </p>
+            <p className="text-gray-500 text-[17px] font-semibold">
+              Click to Generate the Code{" "}
+            </p>
 
-          <button
-            className="generate flex items-center p-4 rounded-lg mt-3  gap-2.5 px-5 transition-all hover:opacity-[0.8]
-                        -auto bg-gradient-to-r from-purple-400  to-purple-600">
-            <BsStars /> Generate</button>
+            <button
+              className="generate flex items-center p-4 rounded-lg mt-3  gap-2.5 px-5 transition-all 
+              hover:opacity-[0.8] bg-linear-to-r from-purple-400  to-purple-600"
+            >
+              <BsStars /> Generate
+            </button>
           </div>
         </div>
-        <div className="left w-[50%] h-[85vh] mt-5 bg-gray-800 rounded-lg" >
-        
+        <div className="left w-[50%] h-[85vh] mt-5 bg-gray-800 rounded-lg">
+          <div className="skeleton w-full h-full items-center flex flex-col justify-center">
+            <div className="circle p-5 h-[100px] w-[100px] rounded-[50%] flex items-center justify-center text-2xl
+            bg-linear-to-r from-purple-400  to-purple-600 "><HiOutlineCode/></div>
+            <p className="text-[16px] text-gray-500 font-semibold mt-3">Code will generate here</p>
+          </div>
         </div>
       </div>
-      
     </>
   );
 };
